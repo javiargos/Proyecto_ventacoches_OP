f1182139bed333d2d6974410d7ba3bac06f6f45f
eledelga <elena.ddr@hotmail.com>
2020-10-22 11:03:14 +0200

    Describir y organizar R

--- r.R
+++ r.R
@@ -1,9 +1,17 @@
+#Descargar datasets####
 Dts<- read.csv("coches.csv")
 Dts1<- read.csv("coches1.csv")
 
-## entrada de datos ####
+#Resumen trabajo####
+### Nuestro proyecto tenia como objetivo predecir según unos valores----
+### el coche más vendido en Estados Unidos y predecir que coche va a----
+### ser más vendido a futuro según la zona geografica y los gustos del----
+### consumidor. ----
 
-## limpieza ####
-#hola#####
-## outcomes ####
+
+# Entrada de datos ####
+
+# Limpieza ####
+
+# Outcomes ####
 
